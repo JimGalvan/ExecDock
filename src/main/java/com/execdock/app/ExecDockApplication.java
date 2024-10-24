ccb342a90c0b122ebe8b2cd68f096f1ff682c8c5
Jim Galvan <58128571+JimGalvan@users.noreply.github.com>
2024-10-24 00:46:23 -0700

    initial code

---
+++ src/main/java/com/execdock/app/ExecDockApplication.java
@@ -0,0 +1,13 @@
+package com.execdock.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExecDockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExecDockApplication.class, args);
+    }
+
+}
